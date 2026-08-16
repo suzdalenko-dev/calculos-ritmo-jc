e5a5385f4728a269e474f32dad5d65b6f279e07b
Producction <produccion@externalizado.com>
2026-08-16 09:13:56 +0200

    suzdsalenko

---
+++ src/main/java/com/suzdal/ritm/RitmApplication.java
@@ -0,0 +1,13 @@
+package com.suzdal.ritm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RitmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RitmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suzdal/ritm/RitmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suzdal.ritm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RitmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
